66b45ff601d423bc4a01bbaa8429c377f4ca36b9
Jenny Cadin <jenny.cadin@gmail.com>
2022-11-14 01:12:42 -0300

    incorporación SQLite

--- app/src/main/java/com/example/recyclerview/activity/Formulario.java
+++ app/src/main/java/com/example/recyclerview/activity/Formulario.java
@@ -20,10 +20,18 @@ import com.example.recyclerview.model.ItemList;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.util.Log;
+import android.widget.Toast;
 
 public class Formulario extends AppCompatActivity {
 
     private EditText txtUsuario;
+    private EditText ed_nombre,ed_rut,ed_edad,ed_telefono,ed_interes;
+    private Button b_agregar,b_ver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +39,18 @@ public class Formulario extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
 
         txtUsuario = (EditText)findViewById(R.id.txtUsuario);
+        ed_nombre = findViewById(R.id.txtUsuario);
+        ed_rut = findViewById(R.id.txtRut);
+        ed_edad = findViewById(R.id.txtEdad);
+        ed_telefono = findViewById(R.id.txtTelefono);
+        ed_interes = findViewById(R.id.txtInteres);
+
+        b_agregar = findViewById(R.id.btnInsertRegis);
+        b_ver= findViewById(R.id.button8);
 
         Button btnInsertar = findViewById(R.id.btnInsertRegis);
+        Button btnInsertar2 = findViewById(R.id.button8);
+
         btnInsertar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,6 +60,54 @@ public class Formulario extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        btnInsertar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Formulario.this, Leer.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 
+    public void insertar()
+    {
+        try
+        {
+            String nombre = ed_nombre.getText().toString();
+            String rut = ed_rut.getText().toString();
+            String edad = ed_edad.getText().toString();
+            String telefono = ed_telefono.getText().toString();
+            String interes = ed_interes.getText().toString();
+
+            SQLiteDatabase db = openOrCreateDatabase("BD_EJEMPLO", Context.MODE_PRIVATE,null);
+            db.execSQL("CREATE TABLE IF NOT EXISTS persona(id INTEGER PRIMARY KEY AUTOINCREMENT,nombre VARCHAR,apellido VARCHAR,edad VARCHAR)");
+
+            String sql = "insert into persona(nombre,rut,edad,telefono,interes)values(?,?,?,?,?)";
+            SQLiteStatement statement = db.compileStatement(sql);
+            statement.bindString(1,nombre);
+            statement.bindString(2,rut);
+            statement.bindString(3,edad);
+            statement.bindString(2,telefono);
+            statement.bindString(3,interes);
+
+            statement.execute();
+            Toast.makeText(this,"Datos agregados satisfactoriamente, felicitaciones.",Toast.LENGTH_LONG).show();
+
+            ed_nombre.setText("");
+            ed_rut.setText("");
+            ed_edad.setText("");
+            ed_telefono.requestFocus();
+            ed_interes.requestFocus();
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this,"Error, no se guardaron los datos, intente nuevamente.",Toast.LENGTH_LONG).show();
+        }
+    }
 }
+
+
---
+++ app/src/main/java/com/example/recyclerview/activity/Persona.java
@@ -0,0 +1,11 @@
+package com.example.recyclerview.activity;
+
+public class Persona {
+    protected String id;
+    protected String nombre;
+    protected String rut;
+    protected String edad;
+    protected String telefono;
+    protected String interes;
+
+}
